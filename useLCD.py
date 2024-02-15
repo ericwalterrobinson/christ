9b3751a8a9cad02cd8841e1d1150a5c2f8980220
ewrobinson <ericwalterrobinson@gmail.com>
2024-02-15 10:53:10 -0700

    still dont know python

--- useLCD.py
+++ useLCD.py
@@ -12,10 +12,10 @@ def main():
 	lcd.message("Welcome to --->\n  CLS TECH")
 	sleep(5)
 	lcd.clear()
-	if sys.argv and len(sys.argv) == 1:
-		lcd.message(sys.argv[0])
+	if sys.argv and len(sys.argv) == 2:
+		lcd.message(sys.argv[1])
 	elif sys.argv:
-		for msg in sys.argv:
+		for msg in sys.argv[1:]:
 			lcd.message(msg)
 			sleep(5)
 			lcd.clear()
