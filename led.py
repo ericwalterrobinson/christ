5236b050816b579efeec75b2e90c080ac7c13872
ewrobinson <ericwalterrobinson@gmail.com>
2024-02-15 11:01:05 -0700

    led changes

--- led.py
+++ led.py
@@ -1,8 +1,11 @@
 import RPi.GPIO as GPIO
 import time
+import sys
+
 greenPins = [17, 18, 27]
 redPins = [20, 21]
 allPins = [17, 18, 27, 20, 21]
+orderedPins = [17, 21, 18, 20, 27]
 def setup():
    # Set the GPIO modes to BCM Numbering
    GPIO.setmode(GPIO.BCM)
@@ -11,18 +14,27 @@ def setup():
    GPIO.setup(allPins, GPIO.OUT, initial=GPIO.HIGH)
 # Define a main function for main process
 def main():
-   GPIO.output(redPins, GPIO.HIGH)
-   while True:
-      #print ('...LED ON')
-      # Turn on LED
-      GPIO.output(greenPins, GPIO.LOW)
-      GPIO.output(redPins, GPIO.HIGH)
-      time.sleep(0.5)
-      #print ('LED OFF...')
-      # Turn off LED
-      GPIO.output(redPins, GPIO.LOW)
-      GPIO.output(greenPins, GPIO.HIGH)
-      time.sleep(0.5)
+   if len(sys.argv) == 2:
+      if sys.argv[1] == "loop":
+         GPIO.output(redPins, GPIO.HIGH)
+         while True:
+            #print ('...LED ON')
+            # Turn on LED
+            GPIO.output(greenPins, GPIO.LOW)
+            GPIO.output(redPins, GPIO.HIGH)
+            time.sleep(0.5)
+            #print ('LED OFF...')
+            # Turn off LED
+            GPIO.output(redPins, GPIO.LOW)
+            GPIO.output(greenPins, GPIO.HIGH)
+            time.sleep(0.5)
+      elif sys.argv[1] == "row":
+         for pin in orderedPins:
+            GPIO.output(pin, GPIO.LOW)
+            time.sleep(0.2)
+            GPIO.output(pin, GPIO.HIGH)
+
+
 # Define a destroy function for clean up everything after the script finished
 def destroy():
    # Turn off LED
